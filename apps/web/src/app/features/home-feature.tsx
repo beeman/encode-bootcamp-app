db7e300fd14a8b628985561da235bb1a9a3ed3cf
Bram Borggreve <beeman@beeman.nl>
2023-06-15 15:49:25 +0100

    feat: show wallet connection on home component

--- apps/web/src/app/features/home-feature.tsx
+++ apps/web/src/app/features/home-feature.tsx
@@ -1,9 +1,16 @@
-import { UiDebug, UiPage } from '@encode-bootcamp-app/ui'
+import { UiPage } from '@encode-bootcamp-app/ui'
+import { Alert } from '@mantine/core'
+import { useWallet } from '@solana/wallet-adapter-react'
 
 export function HomeFeature() {
+  const { wallet } = useWallet()
   return (
     <UiPage title="Home">
-      <UiDebug data={['data']} open />
+      {wallet?.adapter?.publicKey ? (
+        <Alert>You are connected to {wallet.adapter?.publicKey?.toString()}</Alert>
+      ) : (
+        <Alert>You are not connected to a wallet</Alert>
+      )}
     </UiPage>
   )
 }
